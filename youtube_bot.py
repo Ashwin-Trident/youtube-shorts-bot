3cece83b472bc128ceb6ea80680ce939b30d7215
Ashwin-Trident <161690285+Ashwin-Trident@users.noreply.github.com>
2026-03-02 16:11:15 +0400

    Create youtube_bot.py

---
+++ youtube_bot.py
@@ -0,0 +1,6 @@
+import os
+
+YT_CLIENT_ID = os.environ.get("CLIENT_ID")
+YT_CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+YT_REFRESH_TOKEN = os.environ.get("REFRESH_TOKEN")
+PEXELS_API_KEY = os.environ.get("PEXELS_API_KEY")
