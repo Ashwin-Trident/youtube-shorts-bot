e7b64241385d6b9b616df1d58f7617528e453590
Ashwin-Trident <161690285+Ashwin-Trident@users.noreply.github.com>
2026-04-08 11:18:11 +0400

    Update youtube_bot.py

--- youtube_bot.py
+++ youtube_bot.py
@@ -135,7 +135,8 @@ def get_hook():
         "Most people do not know this about.",
         "THIS WILL HIT YOU HARD.",
         "YOU NEEDED THIS TODAY.",
-        "YOU ARE TRYING YOUR BEST."
+        "YOU ARE TRYING YOUR BEST.",
+        "Why you feel stuck in life"
     ]
     return random.choice(hooks)
 
